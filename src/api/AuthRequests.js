d6f0e43460924180b33706b54ca72ee188e86c57
ishal <muhammedishal0483@gmail.com>
2022-12-18 03:10:57 +0530

    api set

--- src/api/AuthRequests.js
+++ src/api/AuthRequests.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000/'
+    baseURL: 'https://long-gold-springbok-gown.cyclic.app/'
   });
 
 export const login=(formData)=>instance.post('/signin',formData)
